bcd1d0611154372ba329dc06f5b65b6967d54e4b
Renato Marroquin <renatoj.marroquin@gmail.com>
2015-12-16 10:18:22 +0100

    Adding some queries back

--- src/main/scala/ch/ethz/queries/chb/Q2.scala
+++ src/main/scala/ch/ethz/queries/chb/Q2.scala
@@ -1,47 +1,52 @@
 package ch.ethz.queries.chb
 
+import ch.ethz.TScanQuery
 import ch.ethz.queries.ChQuery
 import ch.ethz.tell._
+import org.apache.spark.sql.SQLContext
 
 /**
  * Ch Query2
  */
-//class Q2 extends ChQuery {
-//
-//  /**
-//   * implemented in children classes and hold the actual query
-//   */
-//  override def execute(st: String, cm: String, cn: Int, cs: Long, mUrl: String): Unit = {
-//    val scc = new TSparkContext(mUrl, className, st, cm, cn, cs)
-//
-//
-//    import org.apache.spark.sql.functions._
-//    import sqlContext.implicits._
-//
-//    // convert an RDDs to a DataFrames
-//    val stk = stockRdd(scc, new ScanQuery, ChTSchema.stockSch)
-//    if (logger.isDebugEnabled) {
-//      logger.debug("[Query2] %s. Tuples:%d".format("stock", stk.count))
-//    }
-//    val stock = stk.toDF()
-//
-//    val spp = supplierRdd(scc, new ScanQuery, ChTSchema.supplierSch)
-//    if (logger.isDebugEnabled) {
-//      logger.debug("[Query2] %s. Tuples:%d".format("supplier", spp.count))
-//    }
-//    val supplier = spp.toDF()
-//
-//    val nn = nationRdd(scc, new ScanQuery, ChTSchema.nationSch)
-//    if (logger.isDebugEnabled) {
-//      logger.debug("[Query2] %s. Tuples:%d".format("nation", nn.count))
-//    }
-//   val nation = nn.toDF()
-//
-//    val rrr = regionRdd(scc, new ScanQuery, ChTSchema.regionSch)
-//    if (logger.isDebugEnabled) {
-//      logger.debug("[Query2] %s. Tuples:%d".format("region", rrr.count))
-//    }
-//    val region = rrr.toDF()
+class Q2 extends ChQuery {
+
+  /**
+   * implemented in children classes and hold the actual query
+   */
+  override def execute(tSparkContext: TSparkContext, sqlContext: SQLContext): Unit = {
+
+    import BufferType._
+    import org.apache.spark.sql.functions._
+    import sqlContext.implicits._
+
+    // convert an RDDs to a DataFrames
+    val stockQry = new TScanQuery("stock", tSparkContext.partNum.value, Big)
+    val stk = stockRdd(tSparkContext, stockQry, ChTSchema.stockSch)
+    if (logger.isDebugEnabled) {
+      logger.debug("[Query2] %s. Tuples:%d".format("stock", stk.count))
+    }
+    val stock = stk.toDF()
+
+    val sppQry = new TScanQuery("supplier", tSparkContext.partNum.value, Big)
+    val spp = supplierRdd(tSparkContext, sppQry, ChTSchema.supplierSch)
+    if (logger.isDebugEnabled) {
+      logger.debug("[Query2] %s. Tuples:%d".format("supplier", spp.count))
+    }
+    val supplier = spp.toDF()
+
+    val nationQry = new TScanQuery("nation", tSparkContext.partNum.value, Small)
+    val nn = nationRdd(tSparkContext, nationQry, ChTSchema.nationSch)
+    if (logger.isDebugEnabled) {
+      logger.debug("[Query2] %s. Tuples:%d".format("nation", nn.count))
+    }
+   val nation = nn.toDF()
+
+    val regionQry = new TScanQuery("region", tSparkContext.partNum.value, Small)
+    val rrr = regionRdd(tSparkContext, regionQry, ChTSchema.regionSch)
+    if (logger.isDebugEnabled) {
+      logger.debug("[Query2] %s. Tuples:%d".format("region", rrr.count))
+    }
+    val region = rrr.toDF()
 //
 //    /**
 //     * Inner query
@@ -50,14 +55,15 @@ import ch.ethz.tell._
 //     *     where mod((s_w_id*s_i_id),10000)=su_suppkey and su_nationkey=n_nationkey
 //     *     and n_regionkey=r_regionkey and r_name like 'Europ%' group by s_i_id) m
 //     */
-//    val minEuQty = stock.join(supplier, (stock("s_w_id")*stock("s_i_id")%10000) === supplier("su_suppkey"))
-//    .join(nation, $"su_nationkey" === nation("n_nationkey"))
-//    .join(region, $"n_regionkey" === region("r_regionkey"))
-//    .filter(region("r_name").startsWith("Europ"))
-//    .groupBy($"s_i_id")
-//    .agg(min($"s_quantity").as("m_s_quantity")).select($"s_i_id".as("m_i_id"), $"m_s_quantity")
-//
-//    val item = itemRdd(scc, new ScanQuery, ChTSchema.itemSch).toDF()
+    val minEuQty = stock.join(supplier, (stock("s_w_id")*stock("s_i_id")%10000) === supplier("su_suppkey"))
+    .join(nation, $"su_nationkey" === nation("n_nationkey"))
+    .join(region, $"n_regionkey" === region("r_regionkey"))
+    .filter(region("r_name").startsWith("Europ"))
+    .groupBy($"s_i_id")
+    .agg(min($"s_quantity").as("m_s_quantity")).select($"s_i_id".as("m_i_id"), $"m_s_quantity")
+
+    val itemQry = new TScanQuery("item", tSparkContext.partNum.value, Big)
+    val item = itemRdd(tSparkContext, itemQry, ChTSchema.itemSch).toDF()
 //
 //    /**
 //     * select su_suppkey, su_name, n_name, i_id, i_name, su_address, su_phone, su_comment
@@ -68,17 +74,16 @@ import ch.ethz.tell._
 //     * order by n_name, su_name, i_id
 //     */
 //    //ToDo push downs
-//    val res = stock
-//      .join(item, $"s_i_id" === item("i_id"))
-//      .join(supplier, (stock("s_w_id")*stock("s_i_id")%10000) === supplier("su_suppkey"))
-//      .join(nation, $"su_nationkey" === nation("n_nationkey"))
-//      .join(region, $"n_regionkey" === region("r_regionkey"))
-//      .filter(item("i_data").endsWith("b"))
-//      .filter(region("r_name").startsWith("Europ"))
-//      .join(minEuQty, (($"i_id" === minEuQty("m_i_id")) && ($"s_quantity" === minEuQty("m_s_quantity"))))
-//      .orderBy(nation("n_name"), supplier("su_name"), item("i_id"))
-//
-//    timeCollect(res, 2)
-//    scc.sparkContext.stop()
-//  }
-//}
+    val res = stock
+      .join(item, $"s_i_id" === item("i_id"))
+      .join(supplier, (stock("s_w_id")*stock("s_i_id")%10000) === supplier("su_suppkey"))
+      .join(nation, $"su_nationkey" === nation("n_nationkey"))
+      .join(region, $"n_regionkey" === region("r_regionkey"))
+      .filter(item("i_data").endsWith("b"))
+      .filter(region("r_name").startsWith("Europ"))
+      .join(minEuQty, (($"i_id" === minEuQty("m_i_id")) && ($"s_quantity" === minEuQty("m_s_quantity"))))
+      .orderBy(nation("n_name"), supplier("su_name"), item("i_id"))
+
+    timeCollect(res, 2)
+  }
+}
